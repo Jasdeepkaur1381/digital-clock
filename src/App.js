714627ee4a566d49162cb7a09cfe75836d69e647
Jasdeepkaur1381 <kjasdeep219@gmail.com>
2023-06-16 04:20:22 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  let newTime = new Date().toLocaleTimeString();
+  let [cTime, setTime] = useState(newTime);
+
+  let col = '#d994f4';
+  let text = "Click Me !!"
+  let [bg, setbg] = useState(col);
+  let [bttext, settext] = useState(text);
+
+  const setClick = () => {
+    let newcol = '#34495e';
+    setbg(newcol);
+    settext("Ouch !!🤨");
+  };
+
+  const setDouble = () => {
+    setbg(col);
+    settext("Ayoo !!😠");
+  };
+
+  const UpdateTime = () => {
+    newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+  setInterval(UpdateTime, 1000);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div style={{ backgroundColor: bg }}>
+        <h1>{cTime}</h1>
+        <button onClick={setClick} onDoubleClick={setDouble}>{bttext}</button>
+      </div>
+    </>
+  )
 }
 
 export default App;
+
+// workng on events now
+//always right your even in CamelCase
+//their are morefunction like on mouse over on mouse leave on mouae enter 
